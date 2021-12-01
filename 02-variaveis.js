9d96329129f04351c37861b97c8fd387e3333e41
Tércio Cretton <32882410+tcretton@users.noreply.github.com>
2021-11-30 23:02:30 -0300

    Update 02-variaveis.js

--- 02-variaveis.js
+++ 02-variaveis.js
@@ -1,10 +1,11 @@
-var b = 'Tercio'
+var b = 'Tercio' //hoisting
 console.log(b)
 
 let a = 10
 a = 30
 console.log(a)
 
+// Definir como const deixa código mais facil de ler
 const firstName = 'Tercio'
 const lastName = 'Cretton'
 const fullName = firstName + ' ' + lastName
