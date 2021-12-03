e1f1ccdf1f009ca9259d471b65678b6e3d178ba0
Tércio Cretton <32882410+tcretton@users.noreply.github.com>
2021-12-02 22:15:06 -0300

    Update 05-hof.js

--- 05-hof.js
+++ 05-hof.js
@@ -1,6 +1,21 @@
 /* 
 * High Order Functions
-* !Alert cdddd
-*
+! Alert
 */
+
+const soma = (a, b) => a + b
+const mult = (a, b) => a * b
+
+const calc = (op, a, b) => op(a,b)
+const selectOp = op => {
+  if (op === '+'{
+    return soma
+  })
+}
+
+const c = calc(soma, 10, 5)
+
+console.log(c)
+
+
 
