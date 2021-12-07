8334c5252d1988e9fc01e3c3e1e9d104978bb1fb
Tércio Cretton <32882410+tcretton@users.noreply.github.com>
2021-12-06 22:45:55 -0300

    Update 05-hof.js

--- 05-hof.js
+++ 05-hof.js
@@ -7,16 +7,15 @@ const soma = (a, b) => a + b
 const mult = (a, b) => a * b
 
 
-
-
 const calc = (op, a, b) => op(a,b)
+
 const selectOp = op => {
-  if (op === '+'{
+  if (op === '+'){
     return soma
-  })
+  }
 }
 
-const c = calc(soma, 10, 5)
+const c = calc (selectOp('+'), 10, 5)
 
 console.log(c)
 
